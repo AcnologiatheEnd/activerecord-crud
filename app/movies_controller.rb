a810179a3d06106a03b78bfa8f3730e108d0ec61
Winston  Chung <18.chung.winston@ctkprep.org>
2019-12-06 00:05:01 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -75,8 +75,8 @@ end
 def can_update_using_update_method
   # Update movie title to "Wat, huh?"
   Movie.create(title: "Wat?")
-  __
-  __
+  inst = Movie.find_by(title: "Wat?")
+  inst.update(title: "Wat, huh?")
 end
 
 def can_update_multiple_items_at_once
